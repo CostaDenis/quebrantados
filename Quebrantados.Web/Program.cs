6d893ad839c2522e52852b653f68a4401bbf0f79
Denis Ferreira <deniscostaf@gmail.com>
2026-08-27 13:07:00 -0300

    cleanup: removendo itens de Account desnecessários

--- Quebrantados.Web/Program.cs
+++ Quebrantados.Web/Program.cs
@@ -31,14 +31,12 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options =>
     {
-        options.SignIn.RequireConfirmedAccount = true;
+        options.SignIn.RequireConfirmedAccount = false;
         options.Stores.SchemaVersion = IdentitySchemaVersions.Version3;
     })
     .AddEntityFrameworkStores<AppDbContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
-
-builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
 var app = builder.Build();
 
